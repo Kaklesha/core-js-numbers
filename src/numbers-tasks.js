242def6172d9160ded1d42919a1e814f781910f6
Kaklesha <k-korni1ov@yandex.ru>
2023-11-20 22:26:21 +0400

    fix: isSafeInteger

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -499,8 +499,8 @@ function getIntegerOnString(str, base) {
  * 3.5      => false
  * 2 ** 53  => false
  */
-function isSafeInteger(/* number */) {
-  throw new Error('Not implemented');
+function isSafeInteger(number) {
+  return number.isSafeInteger;
 }
 
 /**
